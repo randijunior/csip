3dcdc57541781e1de7fe7a002e1ed0bf51f34103
randijunior <randi.junior@55pbx.com.br>
2026-08-17 17:31:43 -0300

    feat(header): making UserAgent field public

--- rssip/src/message/headers/user_agent.rs
+++ rssip/src/message/headers/user_agent.rs
@@ -4,7 +4,7 @@ use crate::error::Result;
 use crate::parser::{HeaderParse, SipParser};
 
 #[derive(Debug, PartialEq, Eq, Clone)]
-pub struct UserAgent(String);
+pub struct UserAgent(pub String);
 
 impl HeaderParse for UserAgent {
     const NAME: &'static str = "User-Agent";
